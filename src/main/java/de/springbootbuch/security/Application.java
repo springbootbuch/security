412e200d2dfe2533f4b0922fe1d7e53db7c2f538
Michael J. Simons <michael@simons.ac>
2017-05-13 20:56:31 +0200

    Add simple thymeleaf view

--- src/main/java/de/springbootbuch/security/Application.java
+++ src/main/java/de/springbootbuch/security/Application.java
@@ -2,6 +2,9 @@ package de.springbootbuch.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Part of springbootbuch.de.
@@ -13,5 +16,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer viewControllers() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addViewControllers(
+				ViewControllerRegistry reg) {
+				reg
+					.addViewController("/")
+					.setViewName("index");
+			}
+		};
 	}
 }
