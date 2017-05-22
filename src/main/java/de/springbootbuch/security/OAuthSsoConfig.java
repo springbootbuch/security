315fa7aa8995279733ffa8afb46498479cde6808
Michael J. Simons <michael@simons.ac>
2017-05-22 17:39:47 +0200

    Add login / logout function

--- src/main/java/de/springbootbuch/security/OAuthSsoConfig.java
+++ src/main/java/de/springbootbuch/security/OAuthSsoConfig.java
@@ -3,6 +3,8 @@ package de.springbootbuch.security;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Part of springbootbuch.de.
@@ -13,6 +15,22 @@ import org.springframework.context.annotation.Profile;
 @Profile("oauth-sso")
 @Configuration
 @EnableOAuth2Sso
-public class OAuthSsoConfig {
+public class OAuthSsoConfig 
+	extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(
+		HttpSecurity http) throws Exception {
+		http
+			.antMatcher("/**")
+				.authorizeRequests()
+			.antMatchers("/", "/login**")
+				.permitAll()
+			.anyRequest()
+				.authenticated()
+			.and()
+				.logout()
+				.logoutSuccessUrl("/")
+				.permitAll();
+	}
 }
