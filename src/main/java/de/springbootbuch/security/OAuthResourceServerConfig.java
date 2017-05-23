5e6a84c376d7d5a12723be072708896796d6d11c
Michael J. Simons <michael@simons.ac>
2017-05-23 18:25:26 +0200

    Add rsource server example

---
+++ src/main/java/de/springbootbuch/security/OAuthResourceServerConfig.java
@@ -0,0 +1,41 @@
+package de.springbootbuch.security;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * Part of springbootbuch.de.
+ *
+ * @author Michael J. Simons
+ * @author @rotnroll666
+ */
+@Profile("oauth")
+@Configuration
+public class OAuthResourceServerConfig {
+
+	@ConditionalOnMissingBean(ResourceServerConfigAlt.class)
+	@Configuration
+	@EnableResourceServer
+	class ResourceServerConfig {
+	}
+
+	@Profile("custom-resourceserver")
+	@Configuration
+	@EnableResourceServer
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	static class ResourceServerConfigAlt extends
+		ResourceServerConfigurerAdapter {
+
+		@Override
+		public void configure(
+			final HttpSecurity http) {
+			// Ihre Konfiguration hier
+		}
+	}
+}
